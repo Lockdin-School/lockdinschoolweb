e272bb4e0e809237cde67ce1058c56800606736b
Somelele Quse <quse@mergenti.com>
2026-07-30 17:15:28 +0200

    feat(deps): add dotenv to handle env variables
    BREAKING CHANGE: previously could read env files

--- src/api/subjects/subjects.ts
+++ src/api/subjects/subjects.ts
@@ -2,11 +2,10 @@ import {api} from "../client.ts";
 import type {AxiosResponse} from "axios";
 import type {SearchSubjectsResponse} from "./models/SubjectResponse.ts";
 
-
+const API_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const searchSubjects = async () => {
     console.log("searching subjects");
-
     const response: AxiosResponse<SearchSubjectsResponse> = await api.get("/subjects/");
 
     if (!response) {
