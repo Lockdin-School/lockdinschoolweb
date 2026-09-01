1a2de9d7053b0e101333bb01ebb9ea72c0dd994c
Somelele Quse <quse@mergenti.com>
2026-09-01 04:13:57 +0200

    s

--- src/routes/_authenticated/dashboard.tsx
+++ src/routes/_authenticated/dashboard.tsx
@@ -23,22 +23,17 @@ function Dashboard() {
     return (
         <div className="w-full h-full flex gap-10 flex-col items-center">
             <AppHeader/>
-            <section className="flex w-[98vw] h-[80vh]">
-                <aside className="w-[20vw] h-full flex flex-col items-start">
-                    {/*<p className="text-xs text-text flex items-center gap-1 border-border py-2 px-3 border">*/}
-                    {/*    Filters <HugeiconsIcon size={20} icon={ArrowDownFreeIcons}/>*/}
-                    {/*</p>*/}
-                </aside>
-                <main className="w-[60vw] gap-5 flex flex-col items-start">
+            <section className="flex flex-col items-center w-[98vw] h-[80vh]">
+                <main className="max-w-7xl max-sm:px-2 w-full gap-5 flex flex-col items-start">
                     <div className="flex flex-col items-start">
                         <p className="w-full text-xs tracking-tight text-bg bg-border border py-2 px-3 border-border self-start">Subjects</p>
                     </div>
-                    <section className="grid gap-3  elevation-1 w-full grid-cols-3">
+                    <section className="grid gap-3 grid-cols-1 w-full lg:grid-cols-3">
                         {
                             subjects.map((subject, index) => (
                                     <a href={`/subjects/${subject.id}`} key={index}
                                           className="text-start gap-y-0 hover:cursor-pointer w-full flex flex-col  border-border ">
-                                        <div className="h-[20vh] mb-2 rounded w-full bg-accent">
+                                        <div className="sm:h-[20vh] h-[25vh] mb-2 rounded w-full bg-accent">
                                             <img src={"/carousel/5.jpg"} alt={subject.title} className="w-full rounded h-full object-center object-cover" />
                                         </div>
                                         <div className="flex gap-x-2 w-full justify-between items-center">
