0d62c9db3218b65f7d81a297e8fe3fcd3e295ec5
Somelele Quse <quse@mergenti.com>
2026-09-07 20:51:51 +0200

    catch absent student profile

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -53,6 +53,7 @@ function AuthenticatedLayout() {
         }
 
         if (
+            !studentProfile &&
             isStudentProfileError &&
             isStudentProfileNotFoundError(studentProfileError) &&
             !isOnboardingRoute
